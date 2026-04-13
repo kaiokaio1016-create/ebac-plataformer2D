fac7348416ab762c3d03070636c8cfa12335d335
KAIO HENRIQUE DE ASSIS SANTOS <kaio.kaio.1016@gmail.com>
2026-04-13 13:43:34 -0300

    add jump to prototype

--- Assets/Prefabs/Scripts/Player/Player.cs
+++ Assets/Prefabs/Scripts/Player/Player.cs
@@ -6,11 +6,19 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
 
     public float speed;
 
+    public float forceJump = 2;
+
     private void Update()
+    {
+        HandleJump();
+        HandleMoviment();
+    }
+
+    private void HandleMoviment()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -21,6 +29,23 @@ public class Player : MonoBehaviour
         {
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+        }
+
+        if (myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+        else if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = Vector2.up * forceJump;
         }
     }
 }
