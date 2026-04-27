750d51aeafd8a3b0207e6e12bfc863bd99a912a5
KAIO HENRIQUE DE ASSIS SANTOS <kaio.kaio.1016@gmail.com>
2026-04-27 17:14:45 -0300

    add

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -53,7 +53,7 @@ public class Player : MonoBehaviour
 
     private bool IsGrounded()
     {
-        Debug.DrawRay(transform.position, -Vector2.up, Color.magenta, distToGround + spaceToGround);
+       
         return Physics2D.Raycast(transform.position, -Vector2.up, distToGround + spaceToGround);
     }
 
@@ -124,7 +124,8 @@ public class Player : MonoBehaviour
 
     private void PlayJumpVFX()
     {
-        if (jumpVFX != null) jumpVFX.Play();
+        VFXManager.Instance.PlayVFXByType(VFXManager.VFXType.JUMP, transform.position);
+        //if (jumpVFX != null) jumpVFX.Play();
     }
 
     private void HandleScaleJump()
